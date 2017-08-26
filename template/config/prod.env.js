ff36f5a90926dd5b9f3335418a004d0f94e02d4e
伍程凯 <wuchengkai0@gmail.com>
2017-08-26 16:53:28 +0800

    prelude

--- template/config/prod.env.js
+++ template/config/prod.env.js
@@ -1,3 +1,7 @@
+var { production: { sso, api } } = require('./urls')
+
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  SSO: JSON.stringify(sso),
+  API: JSON.stringify(api)
 }
---
+++ template/config/url.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testing: {
+    api: '',
+    sso: 'https://ssodev.rajax.me'
+  },
+  production: {
+    api: '',
+    sso: 'https://sso.rajax.me'
+  }
+}
