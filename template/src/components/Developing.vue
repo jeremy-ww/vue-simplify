82f9cbee8fe91c4c6a971f8e0d04c777651914f4
伍程凯 <wuchengkai0@gmail.com>
2017-09-11 11:11:55 +0800

    prelude

---
+++ template/src/components/Developing.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: 'noop',
+
+  functional: true,
+
+  render (createElement, context) {
+    return createElement('div', {
+      class: { 'developing': true }
+    }, '开发中，敬请期待')
+  }
+}
+</script>
+
+<style lang="less">
+.developing {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  color: #888;
+}
+</style>
