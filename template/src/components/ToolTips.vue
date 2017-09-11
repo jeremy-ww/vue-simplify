b8e206f883be49f844c4105c5a9d2ee859c17505
伍程凯 <wuchengkai0@gmail.com>
2017-09-11 10:57:48 +0800

    prelude

---
+++ template/src/components/ToolTips.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: 'tool-tips',
+
+  functional: true,
+
+  props: {
+    content: { type: Array, default () { return [] } },
+    spacing: { type: Boolean, default: true }
+  },
+
+  render (h, { props: { spacing, content } }) {
+    return h('section',
+      { class: { 'tool-tips-spacing': spacing, 'tool-tips': true } }, content.map(v => h('p', v)))
+  }
+}
+</script>
+
+<style lang="less">
+.tool-tips {
+  max-width: 300px;
+
+  &.tool-tips-spacing p:not(:last-child) {
+    margin-bottom: 10px;
+  }
+}
+</style>
