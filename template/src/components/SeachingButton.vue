8fcb538e827992d8e9b624fc5dbe054e10ecb18f
伍程凯 <wuchengkai0@gmail.com>
2017-09-11 11:00:07 +0800

    prelude

---
+++ template/src/components/SeachingButton.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'seaching-button',
+
+  functional: true,
+
+  props: {
+    loadingText: { type: String, default: '查询中' }
+    text: { type: String, default: '查询' },
+    loading: Boolean
+  },
+
+  render (h, context) {
+    return context.props.loading
+      ? h('el-button',
+          { props: { loading: true, type: 'primary' } }, context.props.loadingText)
+      : h('el-button',
+          { props: { type: 'primary', 'native-type': 'submit' } }, context.props.text)
+  }
+}
+</script>
